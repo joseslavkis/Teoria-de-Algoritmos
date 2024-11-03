fdb5cc5dce54cb808afb726456a0a7080cd3b919
joseslavkis <jslavkis@fi.uba.ar>
2024-11-03 13:19:22 -0300

    actualizo algoritmos

--- DivisionYConquista/ej13.py
+++ DivisionYConquista/ej13.py
@@ -1,46 +1,32 @@
-def max_crossing_subarray(arr, inicio, medio, fin):
-    suma_izquierda = float('-inf')
-    suma_temp = 0
-    max_izquierda = medio
-    
-    for i in range(medio, inicio - 1, -1):
-        suma_temp += arr[i]
-        if suma_temp > suma_izquierda:
-            suma_izquierda = suma_temp
-            max_izquierda = i
-
-    suma_derecha = float('-inf')
-    suma_temp = 0
-    max_derecha = medio + 1
-    
-    for i in range(medio + 1, fin + 1):
-        suma_temp += arr[i]
-        if suma_temp > suma_derecha:
-            suma_derecha = suma_temp
-            max_derecha = i
-    
-    return suma_izquierda + suma_derecha, max_izquierda, max_derecha
-
-def max_subarray_rec(arr, inicio, fin):
-    if inicio == fin:
-        return arr[inicio], inicio, inicio
-
-    medio = (inicio + fin) // 2
-
-    suma_izquierda, left_start, left_end = max_subarray_rec(arr, inicio, medio)
-    suma_derecha, right_start, right_end = max_subarray_rec(arr, medio + 1, fin)
-    suma_cruzada, cross_start, cross_end = max_crossing_subarray(arr, inicio, medio, fin)
-
-    if suma_izquierda >= suma_derecha and suma_izquierda >= suma_cruzada:
-        return suma_izquierda, left_start, left_end
-    elif suma_derecha >= suma_izquierda and suma_derecha >= suma_cruzada:
-        return suma_derecha, right_start, right_end
-    else:
-        return suma_cruzada, cross_start, cross_end
-
 def max_subarray(arr):
-    if len(arr) == 0:
-        return []
-    
-    _, inicio, fin = max_subarray_rec(arr, 0, len(arr) - 1)
-    return arr[inicio:fin + 1]
+    return _max_subarray(arr, 0, len(arr) - 1)
+
+def _max_subarray(arr, ini, fin):
+    if ini == fin:
+        return [arr[ini]]
+    medio = (ini + fin) // 2
+    max_izq = _max_subarray(arr, ini, medio)
+    max_der = _max_subarray(arr, medio + 1, fin)
+    max_cross = max_crossing_subarray(arr, ini, medio, fin)
+    return max([max_izq, max_der, max_cross], key=lambda x: sum(x))
+
+def max_crossing_subarray(arr, ini, medio, fin):
+    max_izq_sum = -float('inf')
+    max_izq_i = medio
+    suma_actual = 0
+    for i in range(medio, ini - 1, -1):
+        suma_actual += arr[i]
+        if suma_actual > max_izq_sum:
+            max_izq_sum = suma_actual
+            max_izq_i = i
+
+    max_der_sum = -float('inf')
+    max_der_i = medio + 1
+    suma_actual = 0
+    for i in range(medio + 1, fin + 1):
+        suma_actual += arr[i]
+        if suma_actual > max_der_sum:
+            max_der_sum = suma_actual
+            max_der_i = i
+
+    return arr[max_izq_i:max_der_i + 1]
--- DivisionYConquista/ej8.py
+++ DivisionYConquista/ej8.py
@@ -1,31 +1,28 @@
-def mergear_y_contar(izq, der):
-    ordenado = []
-    contador = 0
-    i = 0
-    j = 0
-    while i < len(izq) and j < len(der):
-        izquierdo = izq[i]
-        derecho = der[j]
-        if izquierdo <= derecho:
-            ordenado.append(izquierdo)
-            i += 1
-            continue
-        ordenado.append(derecho)
-        j += 1
-        contador += len(izq)-i
-    ordenado.extend(izq[i:])
-    ordenado.extend(der[j:])
-    return ordenado, contador
-
-def _contar_inversiones(arreglo, inicio, fin):
-    if inicio >= fin:
-        return [arreglo[inicio]], 0
-    medio = (inicio + fin) // 2
-    izq, contador_izq = _contar_inversiones(arreglo, inicio, medio)
-    der, contador_der = _contar_inversiones(arreglo, medio+1, fin)
-    ordenado, contador = mergear_y_contar(izq, der)
-    return ordenado, contador_izq + contador_der + contador 
-
 def contar_inversiones(A, B):
     _, inversiones = _contar_inversiones(B, 0, len(B)-1)
     return inversiones
+
+def _contar_inversiones(arr, ini, fin):
+    if ini >= fin:
+        return [arr[ini]], 0
+    medio = (ini + fin) // 2
+    izq, cont_izq = _contar_inversiones(arr, ini, medio)
+    der, cont_der = _contar_inversiones(arr, medio+1, fin)
+    ordenado, contador = mergear_y_contar(izq, der)
+    return ordenado, cont_der + cont_izq + contador
+
+def mergear_y_contar(izq, der):
+    ordenado = []
+    i, j, contador = 0, 0, 0
+    while i < len(izq) and j < len(der):
+        izquierdo, derecho = izq[i], der[j]
+        if izquierdo <= derecho:
+            ordenado.append(izquierdo)
+            i += 1
+        else:
+            ordenado.append(derecho)
+            j += 1
+            contador += len(izq) - i
+    ordenado.extend(izq[i:])
+    ordenado.extend(der[j:])
+    return ordenado, contador
